3791e376c9629b1fc910ada6d2353f9b6b851c4e
rhemaolowu <40676360+rhemaolowu@users.noreply.github.com>
2018-06-29 07:48:48 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -3,36 +3,36 @@ const table = $("#pixelCanvas");
 //assigns the color picker and pixel canvas ids to constants
 
 table.on('click', 'td', function(){
-  $(this).css('background-color', color.val());
+    $(this).css('background-color', color.val());
 });
 //when a box is clicked this adds colour
 
 table.on('dblclick', 'td', function () {
-	$(this).css('background-color', '#ffffff');
+	 $(this).css('background-color', '#ffffff');
 });
 //when the box is double-clicked it returns the colour to white
 
 $('#sizePicker').submit(function(event){
-  event.preventDefault();
-  //this preveents the process at the back end
-  makeGrid();
-  // When size is submitted by the user, call makeGrid()
+    event.preventDefault();
+    //this preveents the process at the back end
+    makeGrid();
+    // When size is submitted by the user, call makeGrid()
 });
 
 
 function makeGrid() {
-  table.children().remove();
-  // this clears the grid whenever the submit button is re-clicked
+    table.children().remove();
+    // this clears the grid whenever the submit button is re-clicked
 
-  let row = $('#inputHeight').val();
-  let col = $('#inputWeight').val();
-  // this gets the values of the height and the Width and assigns the table's id to a constant
+    let row = $('#inputHeight').val();
+    let col = $('#inputWeight').val();
+    // this gets the values of the height and the Width and assigns the table's id to a constant
 
 
-  for(var n = 1; n <= row; n ++){
-    table.append('<tr></tr>');
-    for(var m = 1; m <= col; m ++){
-      table.children().last().append("<td></td>");
+    for(var n = 1; n <= row; n ++){
+      table.append('<tr></tr>');
+      for(var m = 1; m <= col; m ++){
+        table.children().last().append("<td></td>");
+      }
     }
-  }
 }
